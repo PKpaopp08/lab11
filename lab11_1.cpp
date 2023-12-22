007d805e1cfe8687248df3089490d900ed3f72d5
PAO <natthachai_bubpa@cmu.ac.th>
2023-12-22 16:53:33 +0700

    finish him !!!!

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,52 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<string>
+#include<ctime>
+#include<cstdlib>
+
+
+using namespace std;
+
+int main(){
+    string grade[10]={"A","B+","B","C+","C","D+","D","F","W"};
+    cout << "Press Enter 3 times to reveal your future.\n";
+    cin.get();
+    cin.get();
+    cin.get();
+    srand(time(0));
+    int x = rand()%9;
+    cout << "You will get "<< grade[x] <<" in this 261102.";
+    
+   
+}
+
+/* int x = rand()%9;
+    if (x==0){
+        cout << "You will get A in this 261102.";
+    }
+    else if (x==1){
+        cout << "You will get B+ in this 261102.";
+    }
+    else if (x==2){
+        cout << "You will get B in this 261102.";
+    }
+    else if (x==3){
+        cout << "You will get C+ in this 261102.";
+    }
+    else if (x==4){
+        cout << "You will get C in this 261102.";
+    }
+    else if (x==5){
+        cout << "You will get D+ in this 261102.";
+    }
+    else if (x==6){
+        cout << "You will get D in this 261102.";
+    }
+    else if (x==7){
+        cout << "You will get F in this 261102.";
+    }
+    else if (x==8){
+        cout << "You will get W in this 261102.";
+    } */
+
+
+
